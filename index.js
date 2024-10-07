2228279bea580667c6461dd6de6c072b8f9867e6
Yousef Almesaeed <yousefalm@Outlook.com>
2024-10-07 13:28:47 +0300

    Finsihed spicy task for BMI calc at lunch break

--- index.js
+++ index.js
@@ -12,15 +12,53 @@ function calculateBMI() {
   let heightToMeters = height / 100;
   const BMI = weight / (heightToMeters * heightToMeters);
 
+  // if (BMI < 18.5) {
+  //   alert("You are underweight");
+  // } else if (BMI >= 18.5 && BMI <= 24.9) {
+  //   alert("Your are healthy Weight");
+  // } else if (BMI >= 25.0 && BMI <= 29.9) {
+  //   alert("You are overweight");
+  // } else if (BMI >= 30) {
+  //   alert("Your are above obesity");
+  // }
+
   // alert(`Your BMI is ${BMI}`);
 
-  if (BMI < 18.5) {
-    alert("You are underweight");
-  } else if (BMI >= 18.5 && BMI <= 24.9) {
-    alert("Your are healthy Weight");
-  } else if (BMI >= 25.0 && BMI <= 29.9) {
-    alert("You are overweight");
-  } else if (BMI >= 30) {
-    alert("Your are above obesity");
+  if (age >= 19 && age <= 24) {
+    if (BMI >= 19 && BMI <= 24) {
+      alert("Your are Healthy");
+    } else {
+      alert("You are not healthy");
+    }
+  } else if (age >= 25 && age <= 34) {
+    if (BMI >= 20 && BMI <= 25) {
+      alert("You are Healthy");
+    } else {
+      alert("you are not Healthy ");
+    }
+  } else if (age >= 35 && age <= 44) {
+    if (BMI >= 21 && BMI <= 26) {
+      alert("You are healthy");
+    } else {
+      alert("you are not healthy");
+    }
+  } else if (age >= 45 && age <= 54) {
+    if (BMI >= 22 && BMI <= 27) {
+      alert("You are healthy");
+    } else {
+      alert("you are not healthy");
+    }
+  } else if (age >= 55 && age <= 64) {
+    if (BMI >= 23 && BMI <= 28) {
+      alert("You are healthy");
+    } else {
+      alert("you are not healthy");
+    }
+  } else if (age >= 55) {
+    if (BMI >= 23 && BMI <= 28) {
+      alert("You are healthy");
+    } else {
+      alert("you are not healthy");
+    }
   }
 }
