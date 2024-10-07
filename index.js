3bd8f54e11eba625990d95b04b641852f988af4a
Yousef Almesaeed <yousefalm@Outlook.com>
2024-10-07 10:24:26 +0300

    Done Challange 2

--- index.js
+++ index.js
@@ -7,5 +7,20 @@ function calculateBMI() {
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
 
-  // You are going to write the code here before the curly braces 👇🏻
+  // You are going to write the code here before the curly braces
+
+  let heightToMeters = height / 100;
+  const BMI = weight / (heightToMeters * heightToMeters);
+
+  // alert(`Your BMI is ${BMI}`);
+
+  if (BMI < 18.5) {
+    alert("You are underweight");
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    alert("Your are healthy Weight");
+  } else if (BMI >= 25.0 && BMI <= 29.9) {
+    alert("You are overweight");
+  } else if (BMI >= 30) {
+    alert("Your are above obesity");
+  }
 }
